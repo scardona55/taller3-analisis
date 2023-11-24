559b7e483068cc83c9b76fa9d87dee270ff07454
scardona55 <sergio.1701910394@ucaldas.edu.co>
2023-11-24 18:48:27 -0500

    taller analisis

---
+++ analisis.py
@@ -0,0 +1,436 @@
+#imports----------------------------------------------------------------
+import pandas as pd
+import numpy as np
+from fractions import Fraction
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from itertools import product
+
+
+#variables----------------------------------------------------------------
+
+
+canales=[ [0,1,1,0,1,1,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0,0,0,1],
+          [0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,0,0,0],
+          [0,1,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0]
+
+          ]
+
+combinaciones_posibles=[]
+combinaciones_canales=[]
+data2=[]
+
+
+
+
+#-----------------------------------------------------------------------------
+
+
+
+#funciones---------------------------------------------------------------------
+
+def obtener_dato_ingresado():
+    global indice_deseado, fila_resultante  # Necesario para modificar las variables globales
+    indice_deseado = str(dato_ingresado.get())
+    fila_resultante = obtener_fila_por_indice(df5, indice_deseado)
+
+#nos devuelve todas las combinaciones de binarios posibles
+def generar_combinaciones_binarias(n):
+    for combinacion in product("01", repeat=n):
+        combinaciones_posibles.append("".join(combinacion))
+
+def cargar_combinaciones_canales():
+  #iterar las veces de items que tenga el primer canal ya que todos tienen la misma cantidad de items
+  for i in range(len(canales[0])):
+      combinacion=[]
+      #necesitamos sacar la combinacion según la cantidad de canales, se agrega cada elemento de la posición, se pasan a string y se agrega a combinaciones canales
+      for j in range(len(canales)):
+        combinacion.append(canales[j][i])
+      combinacion = [str(elemento) for elemento in combinacion]
+      resultado = ''.join(combinacion)
+      combinaciones_canales.append(resultado)
+
+
+def obtener_indices(x):
+  indices=[]
+
+  for j in range(len(combinaciones_canales)):
+    if combinaciones_posibles[x] == combinaciones_canales[j]:
+      indices.append(j)
+
+  return indices
+
+
+def obtener_fila_por_indice(dataframe, indice):
+    try:
+        fila_seleccionada = dataframe.loc[indice]
+        return pd.DataFrame(fila_seleccionada).transpose()  # Transponemos para obtener una fila en lugar de una columna
+    except KeyError:
+        print(f"No se encontró el índice {indice}.")
+        return None
+    
+    
+# Función para mostrar la tabla
+def mostrar_tabla(data):
+    # Crear una nueva ventana (toplevel) para la tabla
+    ventana_tabla = tk.Toplevel(ventana)
+    ventana_tabla.title("Tabla")
+
+    # Crear un Treeview en la nueva ventana
+    tree = ttk.Treeview(ventana_tabla, columns=['estados'] + list(data.columns), show="headings")
+
+    # Configurar las columnas
+    tree.heading('estados', text='estados')  # Columna adicional para mostrar los nombres de las filas
+    for col in data.columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=100)  # Ajusta el ancho de las columnas según tus necesidades
+
+    # Insertar los datos en el Treeview
+    for nombre_fila, row in data.iterrows():
+        tree.insert("", "end", values=[nombre_fila] + list(row))
+
+    # Coloca el Treeview en la nueva ventana
+    tree.pack()
+
+
+def mostrar_tabla_marginizada(data):
+    # Crear una nueva ventana (toplevel) para la tabla
+    ventana_tabla = tk.Toplevel(ventana)
+    ventana_tabla.title("Tabla")
+
+    # Crear un Treeview en la nueva ventana
+    tree = ttk.Treeview(ventana_tabla, columns=['estados'] + list(data.columns), show="headings")
+
+    # Configurar las columnas
+    tree.heading('estados', text='estados')  # Columna adicional para mostrar los nombres de las filas
+    for col in data.columns:
+        tree.heading(col, text=col)
+        tree.column(col, width=100)  # Ajusta el ancho de las columnas según tus necesidades
+
+    # Insertar los datos en el Treeview
+    for nombre_fila, row in data.iterrows():
+        tree.insert("", "end", values=[nombre_fila] + list(row))
+
+    # Coloca el Treeview en la nueva ventana
+    tree.pack()
+
+    
+    
+    
+def grafica_barras_fila_resultante(fila_resultante):
+    # Crear una figura y ejes
+    fig, ax = plt.subplots()
+
+    # Obtener nombres de columnas y valores de la fila resultante
+    columnas = fila_resultante.columns
+    valores = fila_resultante.values.flatten()
+
+    # Crear un gráfico de barras
+    ax.bar(columnas, valores)
+
+    # Configurar etiquetas y título
+    plt.xlabel('Columna')
+    plt.ylabel('Valor en la Fila Resultante')
+    plt.title(f'Gráfico de Barras para la Fila {indice_deseado}')
+
+    # Mostrar el gráfico
+    plt.show()
+    
+def grafica_barras_data_sumada():
+    # Crear una figura y ejes
+    fig, ax = plt.subplots()
+
+    # Obtener nombres de columnas y valores de data_sumada
+    columnas = data_sumada.columns
+    valores = data_sumada.loc[[indice_deseado]].values  # Utilizar el índice deseado
+
+    # Obtener el número de barras
+    num_barras = len(columnas)
+
+    # Crear un rango para la posición de las barras
+    posicion_barras = np.arange(num_barras)
+
+    # Crear un gráfico de barras verticales intercambiando x e y
+    ax.bar(posicion_barras, valores.flatten(), align='center', label=indice_deseado)  # Utilizar flatten para convertir a una dimensión
+
+    # Configurar etiquetas y título
+    plt.ylabel('Valor en Data Sumada')
+    plt.xlabel('Columna')
+    plt.title(f'Gráfico de Barras para Data Sumada - Índice: {indice_deseado}')
+
+    # Mostrar las etiquetas en el eje x
+    ax.set_xticks(posicion_barras)
+    ax.set_xticklabels(columnas)
+
+    # Mostrar la leyenda
+    ax.legend()
+
+    # Mostrar el gráfico
+    plt.show()
+
+
+
+
+def eliminar_caracter_columnas(dataframe, indice_a_eliminar):
+    # Iterar sobre todas las columnas del DataFrame
+    for columna in dataframe.columns:
+        # Obtener el valor actual de la columna
+        valor_original = columna
+
+        # Eliminar el índice especificado
+        nuevo_valor = valor_original[:indice_a_eliminar] + valor_original[indice_a_eliminar + 1:]
+
+        # Renombrar la columna en el DataFrame
+        dataframe = dataframe.rename(columns={columna: nuevo_valor})
+
+    return dataframe
+  
+def sumar_columnas_repetidas(dataframe):
+    # Agrupar por nombre de columna y sumar los valores
+    dataframe_sumado = dataframe.groupby(dataframe.columns, axis=1).sum()
+
+    return dataframe_sumado
+
+def obtener_numero_ingresado():
+    global indice_a_eliminar, data_marginizada, data_sumada  # Necesario para modificar las variables globales
+    indice_a_eliminar = int(numero_ingresado.get())
+    data_marginizada = eliminar_caracter_columnas(df5, indice_a_eliminar)
+    data_sumada = sumar_columnas_repetidas(data_marginizada)
+    print("la data sumada es ", data_sumada)  # Agrega esta línea para verificar si data_sumada se actualiza correctamente
+
+
+
+
+    
+
+#ejecucción----------------------------------------------------
+
+#obtenemos todas las combinaciones en la variable combinaciones posibles
+generar_combinaciones_binarias(len(canales))
+cargar_combinaciones_canales()
+
+
+
+
+#obtendremos coincidencias
+
+for i in range(len(combinaciones_posibles)):
+  #obtenemos los índices siguientes a las coincidencias
+  indices= obtener_indices(i)
+  for k in canales:
+      datos_tabla=[]
+      coincidencias=0
+      total=0
+      for l in indices:
+        if l+1 != len(canales[0]):
+          if k[l + 1] == 1 :
+              coincidencias=coincidencias+1
+              total=total+1
+          else:
+              total=total+1
+      if(total!=0):
+        datos_tabla.append(coincidencias/total)
+
+      data2.extend(datos_tabla)
+
+
+data2 = [Fraction(numero).limit_denominator() for numero in data2]
+data2 = [data2[i:i+ len(canales)] for i in range(0, len(data2), len(canales))]
+
+
+# Crear nombres para las columnas y filas
+column_names = ['canal{}'.format(i + 1) for i in range(len(canales))]
+index_names = combinaciones_posibles
+
+# Convertir el array de NumPy en un DataFrame de Pandas con nombres
+df = pd.DataFrame(data2, columns=column_names, index=index_names)
+
+# Mostrar el DataFrame resultante
+print(df)
+
+    
+    
+    
+#-ejercicio2-------------------------------
+
+#solución punto 2
+
+data3=[]
+
+for i in range(len(combinaciones_posibles)):
+  #obtenemos los índices siguientes a las coincidencias
+  indices= obtener_indices(i)
+  for k in range(len(combinaciones_posibles)):
+      datos_tabla2=[]
+      coincidencias=0
+      total=0
+      for l in indices:
+        if l+1 != len(combinaciones_canales):
+          if combinaciones_canales[l + 1] == combinaciones_posibles[k] :
+              coincidencias=coincidencias+1
+              total=total+1
+          else:
+              total=total+1
+      if(total!=0):
+        datos_tabla2.append(coincidencias/total)
+
+      data3.extend(datos_tabla2)
+
+data3 = [Fraction(numero).limit_denominator() for numero in data3]
+data3 = [data3[i:i+ len(combinaciones_posibles)] for i in range(0, len(data3), len(combinaciones_posibles))]
+
+# Crear nombres para las columnas y filas
+column_names = combinaciones_posibles
+index_names = combinaciones_posibles
+
+# Convertir el array de NumPy en un DataFrame de Pandas con nombres
+df2 = pd.DataFrame(data3, columns=column_names, index=index_names)
+
+# Mostrar el DataFrame resultante
+print(df2)
+
+
+#ejercicio 3 -------------------------------------------------------------------------------------
+
+data4=[]
+
+for i in range(len(combinaciones_posibles)):
+  #obtenemos los índices siguientes a las coincidencias
+  indices= obtener_indices(i)
+  for k in canales:
+      datos_tabla=[]
+      coincidencias=0
+      total=0
+      for l in indices:
+        if l != 0:
+          if k[l - 1] == 1 :
+              coincidencias=coincidencias+1
+              total=total+1
+          else:
+              total=total+1
+      if(total!=0):
+        datos_tabla.append(coincidencias/total)
+
+      data4.extend(datos_tabla)
+
+
+data4 = [Fraction(numero).limit_denominator() for numero in data4]
+data4 = [data4[i:i+ len(canales)] for i in range(0, len(data4), len(canales))]
+
+
+# Crear nombres para las columnas y filas
+column_names = ['canal{}'.format(i + 1) for i in range(len(canales))]
+index_names = combinaciones_posibles
+
+# Convertir el array de NumPy en un DataFrame de Pandas con nombres
+""" df4 = pd.DataFrame(data4, columns=column_names, index=index_names)
+
+# Mostrar el DataFrame resultante
+print(df4) """
+
+
+#ejercicio4------------------------------------------------
+
+#solución punto 4
+
+data5=[]
+
+for i in range(len(combinaciones_posibles)):
+  #obtenemos los índices siguientes a las coincidencias
+  indices= obtener_indices(i)
+  for k in range(len(combinaciones_posibles)):
+      datos_tabla2=[]
+      coincidencias=0
+      total=0
+      for l in indices:
+        if l != 0:
+          if combinaciones_canales[l - 1] == combinaciones_posibles[k] :
+              coincidencias=coincidencias+1
+              total=total+1
+          else:
+              total=total+1
+      if(total!=0):
+        datos_tabla2.append(coincidencias/total)
+
+      data5.extend(datos_tabla2)
+
+data5 = [Fraction(numero).limit_denominator() for numero in data5]
+data5 = [data5[i:i+ len(combinaciones_posibles)] for i in range(0, len(data5), len(combinaciones_posibles))]
+
+
+
+# Crear nombres para las columnas y filas
+column_names = combinaciones_posibles
+index_names = combinaciones_posibles
+
+# Convertir el array de NumPy en un DataFrame de Pandas con nombres
+df5 = pd.DataFrame(data5, columns=column_names, index=index_names)
+
+# Mostrar el DataFrame resultante
+print(df5)
+
+      
+    
+#Marginalizar--------------------------------------------------------------- 
+
+
+
+
+
+#ventana-----------------------------------------------------------------------------
+ventana = tk.Tk()
+ventana.title("Proyecto Analisis")
+
+dato_ingresado = tk.StringVar()
+numero_ingresado = tk.DoubleVar()
+
+
+# Crear el campo de entrada
+campo_entrada = tk.Entry(ventana, textvariable=dato_ingresado)
+campo_entrada.pack()
+
+campo_numero = tk.Entry(ventana, textvariable=numero_ingresado)
+campo_numero.pack()
+
+# Botón para obtener el dato ingresado
+boton_obtener_dato = tk.Button(ventana, text="Obtener Dato", command=obtener_dato_ingresado)
+boton_obtener_dato.pack()
+
+# Botón para obtener el número ingresado
+boton_obtener_numero = tk.Button(ventana, text="Obtener numero", command=obtener_numero_ingresado)
+boton_obtener_numero.pack()
+
+
+# Botón para mostrar la tabla
+boton_mostrar_tabla = tk.Button(ventana, text="Mostrar Tabla", command=lambda:mostrar_tabla(df5))
+boton_mostrar_tabla.pack()
+
+
+# Botón para mostrar la tabla marginizada
+boton_mostrar_tabla_marginizada = tk.Button(ventana, text="Mostrar Tabla marginizada", command=lambda:mostrar_tabla_marginizada(data_sumada))
+boton_mostrar_tabla_marginizada.pack()
+
+# Botón para mostrar la gráfica de barras de data_sumada
+boton_grafica_barras_data_sumada = tk.Button(ventana, text="Gráfico de Barras para Data Sumada", command=grafica_barras_data_sumada)
+boton_grafica_barras_data_sumada.pack()
+
+""" boton_archivo = tk.Button(ventana, text="Cargar Archivo", command=hacer_clic)
+boton_archivo.pack() """
+
+# Botón para mostrar el gráfico de barras para la fila resultante
+boton_grafica_barras_fila_resultante = tk.Button(ventana, text="Gráfico de Barras para la Fila ", command=lambda: grafica_barras_fila_resultante(fila_resultante))
+boton_grafica_barras_fila_resultante.pack()
+
+
+
+etiqueta = tk.Label(ventana, text="")
+etiqueta.pack()
+
+
+ventana.mainloop()
+
+
+
+
